c43809e72206debb443d88fbccebd6d16a00eafe
Dev Keshwani <dev.keshwani345@gmail.com>
2025-07-10 23:59:50 +0530

    feat: implement main function and webhook processing logic with Redis subscription

--- webhook/main.go
+++ webhook/main.go
@@ -1,1 +1,35 @@
-package webhook
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"dkadev.xyz/captainhook/queue"
+	redisClient "dkadev.xyz/captainhook/redis"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: "",
+		DB:       0,
+	})
+
+	webhookQueue := make(chan redisClient.WebHookPayload, 100)
+
+	go queue.ProcessHooks(ctx, webhookQueue)
+	
+	err := redisClient.Subscribe(ctx, client, webhookQueue)
+	if err != nil {
+		log.Println("Error:", err)
+	}
+
+	select {}
+
+}
--- webhook/queue/worker.go
+++ webhook/queue/worker.go
@@ -1,1 +1,43 @@
 package queue
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"dkadev.xyz/captainhook/sender"
+	redisClient "dkadev.xyz/captainhook/redis"
+)
+
+func ProcessHooks(ctx context.Context, webhookQueue chan redisClient.WebHookPayload) {
+	for payload := range webhookQueue {
+		go func(p redisClient.WebHookPayload) {
+			backoffTime := time.Second
+			maxBackoffTime := time.Hour
+			retries := 0
+			maxRetries := 5
+
+			for {
+				err := sender.SendWebhook(p.Data, p.Url, p.WebhookId)
+				if err == nil {
+					break
+				}
+				log.Println("Error sending webhook:", err)
+
+				retries++
+				if retries >= maxRetries {
+					log.Println("Max retries reached. Giving up on webhook:", p.WebhookId)
+					break
+				}
+
+				time.Sleep(backoffTime)
+
+				backoffTime *= 2
+				if backoffTime > maxBackoffTime {
+					backoffTime = maxBackoffTime
+				}
+
+			}
+		}(payload)
+	}
+}
